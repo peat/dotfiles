d15fe4e5167ee50b7bb250d9d40633f942e930fb
Peat Bakke <peat@peat.org>
2022-07-06 11:44:32 -0700

    Bootstrap script for macos homebrew

---
+++ brew-bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# install homebrew; command line from brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# make sure brew is in path
+export PATH=/usr/local/bin:$PATH
+
+# packages
+brew install calc htop macvim node plantuml rbenv telnet
